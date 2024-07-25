05a9c43fd234a57bdb22dac20e3cae2f226934c8
Dezmond152 <rossomahavit@gmail.com>
2024-07-25 20:54:14 +0300

    commit 25.07.2024

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,8 @@
-function 
+const rows = document.querySelectorAll(".row");
+console.log(rows);
+rows.forEach((row) => {
+    row.addEventListener("click", () => {
+        rows.forEach((e) => e.setAttribute("aria-selected", "false"));
+        row.setAttribute("aria-selected", "true");
+    });
+});
