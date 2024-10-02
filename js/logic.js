efa3962f5879a81d38ef9b9f631cd827b28bda9d
Dezmond152 <rossomahavit@gmail.com>
2024-10-02 23:03:02 +0300

    02.10 desgn fix \ js seprate logic

---
+++ js/logic.js
@@ -0,0 +1,49 @@
+const contentMenuFlex = document.getElementById("content_menu_flex_id");
+
+
+export function closePastContentMenu() {
+  if (contentMenuFlex.childElementCount > 3) {
+    setTimeout(() => contentMenuFlex.firstElementChild.remove(), 400);
+  }
+}
+
+export function openMusicInfo() {
+  let htmlPattern = `
+  <div class="content_menu_animation" id="content_menu_animation_id">
+    <div class="content_menu_frame_red">
+      <div class="content_menu_frame_gray">
+        <div class="content_flex">
+          <div class="content_menu_top_bar">
+            <img class="music_banner" src="../images/banner.jpg">
+          </div>
+          <div class="content_info_bar_flex">
+            <div class="content_song_name">MY TIME</div>
+            <div class="decor_gap">-</div>
+            <div class="content_song_author">BO EN</div>
+          </div>
+          <div class="song_lyrics">
+            Test
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+
+  if (contentMenuFlex.childElementCount < 5) {
+    contentMenuFlex.insertAdjacentHTML("beforeend", htmlPattern);
+  } else {
+    console.log("лимит, сука");
+  }
+}
+
+document.querySelectorAll("input").forEach((input) => {
+  input.addEventListener("input", function () {
+    this.classList.add("scale-up");
+
+    clearTimeout(this.animationTimeout);
+    this.animationTimeout = setTimeout(() => {
+      this.classList.remove("scale-up");
+    }, 90);
+  });
+});
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-let contentMenuFlex = document.getElementById("content_menu_flex_id");
 const rowList = document.querySelectorAll(".row");
+
+import { closePastContentMenu, openMusicInfo } from "./logic.js";
 
 rowList.forEach((row) => {
   row.addEventListener("click", () => {
@@ -11,49 +12,20 @@ rowList.forEach((row) => {
   });
 });
 
-function closePastContentMenu() {
-  if (contentMenuFlex.childElementCount > 3) {
-    setTimeout(() => contentMenuFlex.firstElementChild.remove(), 400);
-  }
-}
 
-function openMusicInfo() {
-  let htmlPattern = `
-  <div class="content_menu_animation" id="content_menu_animation_id">
-    <div class="content_menu_frame_red">
-      <div class="content_menu_frame_gray">
-        <div class="content_flex">
-          <div class="content_menu_top_bar">
-            <img class="music_banner" src="../images/banner.jpg">
-          </div>
-          <div class="content_info_bar_flex">
-            <div class="content_song_name">MY TIME</div>
-            <div class="decor_gap">-</div>
-            <div class="content_song_author">BO EN</div>
-          </div>
-          <div class="song_lyrics">
-            Test
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  `;
 
-  if (contentMenuFlex.childElementCount < 5) {
-    contentMenuFlex.insertAdjacentHTML("beforeend", htmlPattern);
+const api = "https://jsonplaceholder.typicode.com/users";
+
+
+
+async function showClientList() {
+  const response = await fetch(api)
+  if (response.ok) {
+    const data = await response.json();
+    console.log(data);
   } else {
-    console.log("лимит, сука");
+    console.log("Error HTTP: " + response.status);
   }
 }
+showClientList();
 
-document.querySelectorAll("input").forEach((input) => {
-  input.addEventListener("input", function () {
-    this.classList.add("scale-up");
-
-    clearTimeout(this.animationTimeout);
-    this.animationTimeout = setTimeout(() => {
-      this.classList.remove("scale-up");
-    }, 90);
-  });
-});
