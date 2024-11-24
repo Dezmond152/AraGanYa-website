7cc54158ea723d1be9f774ba5e28c60875077b8e
Dezmond152 <rossomahavit@gmail.com>
2024-11-24 18:09:02 +0200

    23.11.2024 separete js logic in 2 file

--- js/logic.js
+++ js/logic.js
@@ -1,14 +1,128 @@
+export { deletePastContentMenu, createMusicInfo, createrRowInteraction, rowclickPlay, hoverSFXPlay};
+
 const contentMenuFlex = document.getElementById("content_menu_flex_id");
+const songVolumeRange = document.getElementById("song_Volume");
+const sfxVolumeRange = document.getElementById("sfx_Volume");
+const defaultButton = document.getElementById("default_button");
+const stopButton = document.getElementById("stop_button");
+const tuneMenuImg = document.querySelector(".tune_menu_img");
+const closeButton = document.querySelector(".close_zone");
+const tuneMenu = document.querySelector(".tune_menu");
+const rowList = document.querySelectorAll(".row");
+
+const rowclick = new Audio("/sfx/row_clickSFX.wav");
+const hoverSFX = new Audio("/sfx/hoverSFX.wav");
+
+rowclick.volume = 0.5;
+hoverSFX.volume = 0.5;
+let currentAudio = null;
+let currentAudioVolume = 50;
+
+let curentSongBanner = "bo";
+let curentSongName = "bo";
+let curentSongAuthor = "bo";
+let curentSongLyrics = "bo";
+
+
+
+function createrRowInteraction(){
+  rowList.forEach((row) => {
+    row.addEventListener("click", () => {
+      rowList.forEach((row) => row.setAttribute("aria-selected", "false"));
+      row.setAttribute("aria-selected", "true");
+  
+      let songPath = row.getAttribute("data-path");
+      let bannerPath = row.getAttribute("banner-path");
+      let lyrics = row.getAttribute("lyrics-path");
+      
+      console.log(songPath);
+      console.log(bannerPath);
+      console.log(lyrics);
+  
+      if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+      }
+  
+      currentAudio = new Audio(songPath);
+      currentAudio.volume = currentAudioVolume / 100;
+      currentAudio.loop = true;
+      currentAudio.play();
+   
+      rowclickPlay();            
+      createMusicInfo();        
+      deletePastContentMenu();    
+    }); 
+  
+    row.addEventListener("mouseenter", () => {
+      hoverSFXPlay();
+    });
+  });
+};
+
+
+songVolumeRange.addEventListener("input", () => {
+  currentAudioVolume = songVolumeRange.value;
+  if (currentAudio) {
+    currentAudio.volume = currentAudioVolume / 100;
+  }
+});
+
+//Блок SfX
+function rowclickPlay() {
+  rowclick.currentTime = 0;
+  rowclick.play();
+}
+
+function hoverSFXPlay() {
+  hoverSFX.currentTime = 0;
+  hoverSFX.play();
+}
+
+sfxVolumeRange.addEventListener("input", () => {
+  const sfxValue = sfxVolumeRange.value / 100;
+  hoverSFX.volume = sfxValue;
+  rowclick.volume = sfxValue;
+});
+
+//Меню регулировки звука
+tuneMenuImg.addEventListener("click", () => {
+  tuneMenu.classList.toggle("active");
+  tuneMenuImg.classList.toggle("active");
+  rowclickPlay();
+});
+
+closeButton.addEventListener("click", () => {
+  tuneMenu.classList.remove("active");
+  rowclickPlay();
+});
+
+defaultButton.addEventListener("click", () => {
+  hoverSFX.volume = 0.5; 
+  rowclick.volume = 0.5; 
+  if (currentAudio) { 
+    currentAudio.volume = 0.5;
+    currentAudioVolume = 50;
+  }
+  songVolumeRange.value = 50;
+  sfxVolumeRange.value = 50;
+});
+
+stopButton.addEventListener("click", () => {
+  if(currentAudio){
+    currentAudio.pause();
+    currentAudio.currntTime = 0;
+  }
+});
+
+
+
 
 function deletePastContentMenu() {
   if (contentMenuFlex.childElementCount > 3) {
     setTimeout(() => contentMenuFlex.firstElementChild.remove(), 400);
   }
 }
-
-let SEX0 = "#";
-let SEX1 = "MY TIME";
-let SEX2 = "bo en";
 
 function createMusicInfo() {
   let htmlPattern = `
@@ -17,14 +131,14 @@ function createMusicInfo() {
       <div class="content_menu_frame_gray">
         <div class="content_flex">
           <div class="content_menu_top_bar">
-            <img class="music_banner" src="${SEX0}">
+            <img class="music_banner" src="${curentSongBanner}">
           </div>
           <div class="content_info_bar_flex">
-            <div class="content_song_name">${SEX1}</div>
-            <div class="content_song_author">${SEX2}</div>
+            <div class="content_song_name">${curentSongName}</div>
+            <div class="content_song_author">${curentSongAuthor}</div>
           </div>
           <div class="song_lyrics">
-            Test
+            ${curentSongLyrics}
           </div>
         </div>
       </div>
@@ -39,25 +153,6 @@ function createMusicInfo() {
   }
 }
 
-document.querySelectorAll("input").forEach((input) => {
-  input.addEventListener("input", function () {
-    this.classList.add("scale-up");
-
-    clearTimeout(this.animationTimeout);
-    this.animationTimeout = setTimeout(() => {
-      this.classList.remove("scale-up");
-    }, 90);
-  });
-});
-
-
-
-
-
-function pullSongFromBd(){
-
-
-}
 
 
 
@@ -67,4 +162,18 @@ function pullSongFromBd(){
 
 
 
-export { deletePastContentMenu, createMusicInfo, pullSongFromBd};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,116 +1,18 @@
-import { deletePastContentMenu, createMusicInfo, pullSongFromBd} from "./logic.js";
+import { deletePastContentMenu, createMusicInfo, createrRowInteraction, rowclickPlay, hoverSFXPlay } from "./logic.js";
 
-const songVolumeRange = document.getElementById("song_Volume");
-const sfxVolumeRange = document.getElementById("sfx_Volume");
-const defaultButton = document.getElementById("default_button");
-const stopButton = document.getElementById("stop_button");
-const searchBar = document.getElementById("search_bar");
+createrRowInteraction();
 
-
-
-
-const rowclick = new Audio("/sfx/row_clickSFX.wav");
-const hoverSFX = new Audio("/sfx/hoverSFX.wav");
-
-rowclick.volume = 0.5;
-hoverSFX.volume = 0.5;
-let currentAudio = null;
-let currentAudioVolume = 50;
-
-const rowList = document.querySelectorAll(".row");
-
-
-rowList.forEach((row) => {
-  row.addEventListener("click", () => {
-    rowList.forEach((row) => row.setAttribute("aria-selected", "false"));
-    row.setAttribute("aria-selected", "true");
-
-    let songPath = row.getAttribute("data-path");
-
-    if (currentAudio) {
-      currentAudio.pause();
-      currentAudio.currentTime = 0;
-    }
-
-    currentAudio = new Audio(songPath);
-    currentAudio.volume = currentAudioVolume / 100;
-    currentAudio.loop = true;
-    currentAudio.play();
- 
-    rowclickPlay();            
-    createMusicInfo();        
-    deletePastContentMenu();    
-  }); 
-
-  row.addEventListener("mouseenter", () => {
-    hoverSFXPlay();
-  });
-});
-
-songVolumeRange.addEventListener("input", () => {
-  currentAudioVolume = songVolumeRange.value;
-  if (currentAudio) {
-    currentAudio.volume = currentAudioVolume / 100;
+//Обновление списка песен при нажатии имени сайта
+document.getElementById("site_name").addEventListener("click", async () => {
+  async function fetchHTML(url) {
+    const response = await fetch(url);
+    const html = await response.text();
+    return html; 
   }
-});
-
-
-//Блок SfX
-function rowclickPlay() {
-  rowclick.currentTime = 0;
-  rowclick.play();
-}
-
-function hoverSFXPlay() {
-  hoverSFX.currentTime = 0;
-  hoverSFX.play();
-}
-
-sfxVolumeRange.addEventListener("input", () => {
-  const sfxValue = sfxVolumeRange.value / 100;
-  hoverSFX.volume = sfxValue;
-  rowclick.volume = sfxValue;
-});
-
-//Меню регулировки звука
-const tuneMenuImg = document.querySelector(".tune_menu_img");
-const closeButton = document.querySelector(".close_zone");
-const tuneMenu = document.querySelector(".tune_menu");
-
-tuneMenuImg.addEventListener("click", () => {
-  tuneMenu.classList.toggle("active");
-  tuneMenuImg.classList.toggle("active");
-  rowclickPlay();
-});
-
-closeButton.addEventListener("click", () => {
-  tuneMenu.classList.remove("active");
-  rowclickPlay();
-});
-
-defaultButton.addEventListener("click", () => {
-  hoverSFX.volume = 0.5; 
-  rowclick.volume = 0.5; 
-  if (currentAudio) { 
-    currentAudio.volume = 0.5;
-    currentAudioVolume = 50;
-  }
-  songVolumeRange.value = 50;
-  sfxVolumeRange.value = 50;
-});
-
-stopButton.addEventListener("click", () => {
-  if(currentAudio){
-    currentAudio.pause();
-    currentAudio.currntTime = 0;
-  }
-});
-
-//Строка поиска
-searchBar.addEventListener("search", () => {
-  let searchResault = searchBar.value;
-  console.log(searchResault);
-  pullSongFromBd(searchResault);
+  const htmlPattern = await fetchHTML('http://localhost:3000/sfx');
+  const musicListContainer = document.querySelector("#music_list_container");
+  musicListContainer.innerHTML = htmlPattern;
+  createrRowInteraction();
 });
 
 
@@ -122,24 +24,10 @@ conformationButton.addEventListener("click", () => {
 });
 
 
-
-
-
-//Обновление списка песен при нажатии имени сайта
-document.getElementById("site_name").addEventListener("click", async () => {
-  
-  async function fetchHTML(url) {
-    const response = await fetch(url);
-    const html = await response.text();
-    return html; 
-  }
-
-  const htmlPattern = await fetchHTML('http://localhost:3000/sfx');
-
-  const musicListContainer = document.querySelector("#music_list_container");
-  
-  musicListContainer.innerHTML = htmlPattern;
-
+//Строка поиска
+const searchBar = document.getElementById("search_bar");
+searchBar.addEventListener("search", () => {
+  let searchResault = searchBar.value;
+  console.log(searchResault);
+  // pullSongFromBd(searchResault);
 });
-
-
