a693775da5056351671eea47cc91bfa4bd03a2c6
Dezmond152 <rossomahavit@gmail.com>
2024-11-27 18:47:16 +0200

    27.11.2024 serach bar frontend logic

--- server/servLogic.js
+++ server/servLogic.js
@@ -1,7 +1,7 @@
-module.exports = {getHTMLrandSong, findSonginDB};
+module.exports = { getHTMLrandSong, findSonginDB };
 const mysql2 = require("mysql2/promise");
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
 const { log, error } = require("console");
 const { waitForDebugger } = require("inspector");
 const { resolve } = require("path");
@@ -12,7 +12,6 @@ const dbConfig = {
   password: "dezmond152",
   database: "araganya",
 };
-
 
 const rowPattern = (id, file, banner, lyrics, icon, name, author) => `
 <div class="row" id="${id}" data-path="${file}" banner-path="${banner}" lyrics-path="${lyrics}" aria-selected="false">
@@ -25,15 +24,13 @@ const rowPattern = (id, file, banner, lyrics, icon, name, author) => `
   </div>
 </div>`;
 
-
-
 async function getHTMLrandSong() {
   const songsRangeQuery = `SELECT COUNT(*) AS total FROM songs`;
-  let wantRange = 15; 
+  let wantRange = 15;
 
   const connection = await mysql2.createConnection(dbConfig);
   const [result] = await connection.query(songsRangeQuery);
-  
+
   const allSongs = result[0].total;
 
   const randomNumbers = new Set();
@@ -43,37 +40,56 @@ async function getHTMLrandSong() {
   }
 
   const randomNumbersArray = Array.from(randomNumbers);
-  
-  const randomSongsQuery = `SELECT id, file, banner, lyrics, icon, name, author FROM songs WHERE id IN (${randomNumbersArray.join(',')})`;
+
+  const randomSongsQuery = `SELECT id, file, banner, lyrics, icon, name, author FROM songs WHERE id IN (${randomNumbersArray.join(
+    ","
+  )})`;
   const [randomSongsArray] = await connection.query(randomSongsQuery);
-  
-  const htmlPatern = randomSongsArray.map((song) => rowPattern(song.id, song.file, song.banner, song.lyrics, song.icon, song.name, song.author )).join('');
+
+  const htmlPatern = randomSongsArray
+    .map((song) =>
+      rowPattern(
+        song.id,
+        song.file,
+        song.banner,
+        song.lyrics,
+        song.icon,
+        song.name,
+        song.author
+      )
+    )
+    .join("");
   const indexPath = path.join(__dirname, "../views", "index.html");
-  let indexHTML = fs.readFileSync(indexPath, 'utf8');
+  let indexHTML = fs.readFileSync(indexPath, "utf8");
   indexHTML = indexHTML.replace('<div class ="replase"></div>', htmlPatern);
 
   await connection.end();
-  return {indexHTML, htmlPatern};
-};
+  return { indexHTML, htmlPatern };
+}
 
-function normalizeString(str){
-  return str.trim().replace(/\s+/g, '').toLowerCase();
-};
+function normalizeString(str) {
+  return str.trim().replace(/\s+/g, "").toLowerCase();
+}
 
 async function findSonginDB(inputData) {
   const normalizedString = await normalizeString(inputData);
-  const findRequest =`SELECT name, author FROM songs WHERE LOWER(name) LIKE ? OR LOWER(author) LIKE ?`;
+  const findRequest = `SELECT name, author FROM songs WHERE LOWER(name) LIKE ? OR LOWER(author) LIKE ?`;
 
   const connection = await mysql2.createConnection(dbConfig);
   const [result] = await connection.query(findRequest, [`%${normalizedString}%`, `%${normalizedString}%`]);
 
-  const songsName = result.map(result => `'${result.name}'`);
+  if (result.length === 0) {
+    await connection.end();
+    return null;
+  }
+
+  const songsName = result.map(result => `'${result.name}'`).join(',');
 
   const pullRequest = `SELECT id, file, banner, lyrics, icon, name, author FROM songs WHERE name IN (${songsName})`;
   const [SongsArray] = await connection.query(pullRequest);
-  const pulledSongs = SongsArray.map((song) => rowPattern(song.id, song.file, song.banner, song.lyrics, song.icon, song.name, song.author )).join('');
+
+  const pulledSongs = SongsArray.map((song) => rowPattern(song.id, song.file, song.banner, song.lyrics, song.icon, song.name, song.author) ).join('');
 
   await connection.end();
   return pulledSongs;
 };
-
